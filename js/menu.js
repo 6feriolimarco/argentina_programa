68297a535ce3d846fdd97414965181c7ced5fc52
Francisco Ferioli Marco <franciscoferiolimarco@protonmail.com>
2023-02-01 17:22:34 +0100

    Unifique scripts y agregue la funcion cerrar menu

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,59 @@
+function abrirMenu() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+
+	function scrollIndicatorContainer() {
+		var sir = document.getElementById("pContainer");
+		if (sir.className === "progress-container") {
+			sir.className += " responsive";
+		} else {
+			sir.className = "progress-container";
+		}
+	}
+
+	function scrollIndicatorBar() {
+		var sir = document.getElementById("myBar");
+		if (sir.className === "progress-bar") {
+			sir.className += " responsive";
+		} else {
+			sir.className = "progress-bar";
+		}
+	}
+
+}
+
+function cerrarMenu() {
+	var x = document.getElementById("myTopnav");
+	if (x.className === "topnav responsive") {
+		x.className = "topnav";
+	} else {
+		x.className = "topnav responsive";
+	}
+
+	function scrollIndicatorContainer() {
+		var sir = document.getElementById("pContainer");
+		if (sir.className === "progress-container responsive") {
+			sir.className = "progress-container";
+		} else {
+			sir.className = "progress-container";
+		}
+	}
+
+	function scrollIndicatorBar() {
+		var sir = document.getElementById("myBar");
+		if (sir.className === "progress-bar responsive") {
+			sir.className = "progress-bar";
+		} else {
+			sir.className = "progress-bar";
+		}
+	}
+
+}
+
 function abrirPestania(evt, nombrePestania) {
 	// Declare all variables
 	var i, tabcontent, tablinks;
@@ -17,4 +73,7 @@ function abrirPestania(evt, nombrePestania) {
 	// Show the current tab, and add an "active" class to the button that opened the tab
 	document.getElementById(nombrePestania).style.display = "block";
 	evt.currentTarget.className += " active";
-} 
+
+	// Cierra el menu en cuanto se aprieta el boton de alguna de las pestanias
+	cerrarMenu();
+}
